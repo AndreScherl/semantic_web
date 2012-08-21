0e3ca54f2a15d48693eb0f7ec2eb896a4176d974
Andre Scherl <post@schoerlock.de>
2012-08-21 14:58:43 +0200

    updated version info

--- version.php
+++ version.php
@@ -4,5 +4,5 @@ $plugin->version = 2012082100;
 $plugin->cron = 1;
 $plugin->requires = 2010112400; // Moodle 2.0
 $plugin->component = 'block_semantic_web';
-$plugin->release = '1.0 (Build: 2012071601)';
+$plugin->release = '1.1 (Build: 2012082100)';
 $plugin->dependencies = array('block_case_repository' => 2012082100, 'block_user_preferences' => 2012082100);
